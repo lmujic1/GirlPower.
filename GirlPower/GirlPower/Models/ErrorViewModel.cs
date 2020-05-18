a7fa3c841937ea8194f03888677558c120f6bb75
lmujic1 <lmujic1@etf.unsa.ba>
2020-05-18 11:34:03 +0200

    initial

---
+++ GirlPower/GirlPower/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GirlPower.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
